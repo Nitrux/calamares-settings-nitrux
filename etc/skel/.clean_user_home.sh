b16a3b055500312a7ecfddb560b35c66692e8d6b
Alexis López Zubieta <azubieta90@gmail.com>
2017-09-29 11:00:22 -0500

    Remove installer desktop icon in a cleaner way

--- etc/skel/.clean_user_home.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dpkg -s calamares &> /dev/null
-
-if [ $? ]; then
-    find . -iname 'org.nxos.installer.desktop' -delete
-    sed -i 's/source .clean_user_home.sh//g' ~/.bashrc
-    rm .clean_user_home.sh
-fi;
